6b1105b0d3a9df4daafc0a982fe6ee98a37771c1
joaokbmartins <martins.joao1909@gmail.com>
2021-08-08 17:13:44 -0300

    feat: using HttpRequest and HttpSession

--- src/main/java/com/joao/springbootweb/IndexController.java
+++ src/main/java/com/joao/springbootweb/IndexController.java
@@ -1,5 +1,8 @@
 package com.joao.springbootweb;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@RequestMapping("home")
-	public String index() {
+	public String index(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String type = (String) request.getParameter	("type");
+		System.out.println("Type: " + type);
+		session.setAttribute("type", type);
 		return "index";
 	}
 
--- src/main/webapp/pages/index.jsp
+++ src/main/webapp/pages/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-  ABOBA
+  ABOBA: ${type}
+  <!-- < %=session.getAttribute("type")%> -->
 </body>
 </html>
