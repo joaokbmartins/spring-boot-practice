518ed3424d5bc93b3a1c3a5bc45d354fd9499d15
joaokbmartins <martins.joao1909@gmail.com>
2021-08-16 00:41:27 -0300

    chore: update / delete car added

--- src/main/java/com/joao/springbootweb/controllers/CarRestController.java
+++ src/main/java/com/joao/springbootweb/controllers/CarRestController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,23 @@ public class CarRestController {
 
 	@PostMapping("/car")
 	public Car addCar(@RequestBody Car car) {
+		try {
+			return this.carRepository.save(car);
+		} catch (Exception ex) {
+			System.err.println(ex.getStackTrace());
+			return null;
+		}
+	}
+
+	@DeleteMapping("/car/delete")
+	public void deleteCar(@RequestBody int id) {
+		Car car = this.carRepository.findById(id).get();
+		this.carRepository.delete(car);
+	}
+
+	@PutMapping("/car")
+	public Car updateCar(@RequestBody Car car) {
 		return this.carRepository.save(car);
 	}
+
 }
